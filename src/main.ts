8da50492797554b0f1b6f58c36a926cfac935a09
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-09-06 16:17:24 -0500

    Update package dependencies for vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//traductions
+import { createI18n } from 'vue-i18n'
+import es from '../src/locales/es.json'
+import en from '@/locales/en.json'
+
+
+
+
+
+
 const app = createApp(App)
 
 app.use(createPinia())
