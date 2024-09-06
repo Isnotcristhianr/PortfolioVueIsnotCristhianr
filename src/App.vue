19550b9cea155c28a1a997b90fc7189738fd322a
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-09-05 19:44:39 -0500

    instalacion de propyecto base y tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div class="bg-red-500">
       <HelloWorld msg="You did it!" />
 
       <nav>
