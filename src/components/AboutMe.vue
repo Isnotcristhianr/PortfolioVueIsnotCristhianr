72de362e915c73a433b66ea69acf9162c51b37a8
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-09-11 20:05:28 -0500

    Refactorizar componentes y actualizar imágenes de proyectos, corregir ruta de icono de favicon, componente aboutme

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="min-h-screen text-white p-8 flex flex-col justify-center">
+    <div class="max-w-4xl mx-auto flex flex-col md:flex-row items-start md:items-center gap-8">
+      <div class="flex-1">
+        <div class="space-y-4 text-gray-300">
+          <p>
+            IsnotCrishitan es el reflejo de mi ilusión por crecer como profesional dentro de la
+            industria del desarrollo del software. Mi nombre es Cristhian, y soy un desarrollador
+            web y mobile en continua formación y me apasionan los retos.
+          </p>
+          <p>
+            Algunos de mis éxitos incluyen
+            <span class="font-mono text-white"
+              >colaborar con [Empresa/Proyecto] para el desarrollo de [Logro Específico]</span
+            >. Aunque hoy está desaparecido fue un gran avance en el mundo del desarrollo web.
+          </p>
+          <p>
+            Como creador de contenido,
+            <span class="font-mono text-white"
+              >cuento con el canal de habla hispana más visto del mundo en la categoría de Software
+              & Game Development en [Plataforma]</span
+            >. Mi objetivo es mejorar la empleabilidad de la comunidad hispana y el acceso a
+            contenido de calidad.
+          </p>
+        </div>
+      </div>
+      <div class="md:w-1/3">
+        <img src="" alt="Tu foto" class="rounded-lg shadow-2xl w-full" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Icon from '@iconify/vue'
+</script>
+
+<style scoped>
+.font-mono {
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono',
+    'Courier New', monospace;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
 import HeroSection from '../components/HeroSection.vue'
 import timeLine from '../components/timeLine.vue'
 import ProyectsDev from '../components/ProyectsDev.vue'
+import AboutMe from '../components/AboutMe.vue'
 //icons
 import { Icon } from '@iconify/vue'
 </script>
@@ -73,6 +74,14 @@ import { Icon } from '@iconify/vue'
             {{ $t('seemore') }}
           </router-link>
         </div>
+      </div>
+      <!-- About Me -->
+      <div class="max-w-screen-lg mx-auto pl-4 pr-4 pb-5">
+        <h2 class="text-4xl font-bold text-start text-primary mt-10 mb-10 flex items-center">
+          <Icon icon="heroicons-solid:user" width="30" height="30" class="mr-2" />
+          {{ $t('about') }}
+        </h2>
+        <AboutMe />
       </div>
     </div>
   </main>
