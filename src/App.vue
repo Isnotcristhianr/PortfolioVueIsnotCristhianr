16628614e7bbfb904a4eaf8312a1078e6890b1c4
Cristhian David Recalde <crecalde@neaternotes.com>
2024-10-09 19:32:18 -0500

    add: integrate @vercel/analytics for improved site analytics tracking

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 //components
 import NavBar from './components/navbar/NavBar.vue'
 
+//analitics
+import { Analytics } from "@vercel/analytics/react"
+
+
 
 </script>
 
 <template>
   <div class="bg-base-200">
+    <!-- Analytics -->
+    <Analytics />
     <header>
       <!-- nav bar -->
       <NavBar />
